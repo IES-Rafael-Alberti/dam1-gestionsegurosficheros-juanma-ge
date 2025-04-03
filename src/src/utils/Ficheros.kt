342a5b8c9b69a41c29e592461b1703c17f83d85e
juanma-ge <jgomest545@g.educaand.es>
2025-04-03 03:11:53 +0200

    Clase Ficheros

--- src/src/utils/Ficheros.kt
+++ src/src/utils/Ficheros.kt
@@ -1,27 +1,46 @@
 package utils
 
 import model.IExportable
+import ui.IEntradaSalida
+import java.io.File
 
-class Ficheros: IUtilFicheros {
+class Ficheros(private val ui: IEntradaSalida): IUtilFicheros {
 
     override fun leerArchivo(ruta: String): List<String> {
-        TODO("Not yet implemented")
+        return try {
+            File(ruta).readLines()
+        }catch (e: IllegalArgumentException){
+            ui.mostrarError("Error al leer el mensaje.")
+            emptyList()
+        }
     }
 
     override fun agregarLinea(ruta: String, linea: String): Boolean {
-        TODO("Not yet implemented")
+        return try {
+            File(ruta).appendText("$linea\n")
+            true
+        }catch (e: IllegalArgumentException){
+            ui.mostrarError("Error al intentar escribir.")
+            false
+        }
     }
 
     override fun <T : IExportable> escribirArchivo(ruta: String, elementos: List<T>): Boolean {
-        TODO("Not yet implemented")
+        return try {
+            File(ruta).writeText(elementos.joinToString("\n") {it.serializar()} )
+            true
+        }catch (e: IllegalArgumentException){
+            ui.mostrarError("Error al intentar escribir el archivo.")
+            false
+        }
     }
 
     override fun existeFichero(ruta: String): Boolean {
-        TODO("Not yet implemented")
+        return File(ruta).isFile
     }
 
     override fun existeDirectorio(ruta: String): Boolean {
-        TODO("Not yet implemented")
+        return File(ruta).isDirectory
     }
 
 }
